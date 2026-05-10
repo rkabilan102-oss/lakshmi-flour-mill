521ce8c0c67bc55b6a9b767f313af3778cb7b884
rkabilan102-oss <rkabilan102@gmail.com>
2026-05-10 19:41:50 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -73,23 +73,3 @@ function searchProducts() {
 
     });
 }
-// Firebase Import
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-
-import {
-  getAuth,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  signOut
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-
-// Firebase Config
-const firebaseConfig = {
-  apiKey: "AIzaSyDoojx1yz5yXTjx1bWIRZLxKuGSwYTtEv8",
-  authDomain: "lakshmi-flour-mill-36db5.firebaseapp.com",
-  projectId: "lakshmi-flour-mill-36db5",
-  storageBucket: "lakshmi-flour-mill-36db5.firebasestorage.app",
-  messagingSenderId: "537203846814",
-  appId: "1:537203846814:web:6ca24cf9e5815e0e21558f",
-  measurementId: "G-MY6NJDX6X1"
-};
