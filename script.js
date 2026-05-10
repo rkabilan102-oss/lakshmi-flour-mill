04b46884eb37028ab7f64e77d13ceb918d57cec6
rkabilan102-oss <rkabilan102@gmail.com>
2026-05-10 18:07:38 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -54,3 +54,22 @@ function scrollToProducts() {
         behavior: 'smooth'
     });
 }
+function searchProducts() {
+
+    let input = document.getElementById('searchInput').value.toLowerCase();
+
+    let products = document.querySelectorAll('.product-card');
+
+    products.forEach(product => {
+
+        let productName = product.querySelector('h3').innerText.toLowerCase();
+
+        if(productName.includes(input)) {
+            product.style.display = 'block';
+        }
+        else {
+            product.style.display = 'none';
+        }
+
+    });
+}
